28a13d9ab675ca9412ede85b7ae37373c9e896e9
chaoswang <chaoswang000@gmail.com>
2021-09-04 11:29:41 +0800

    update

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,6 @@
 $.ajaxPrefilter(function (options) {
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    options.url = 'http://127.0.0.1:3007' + options.url
     // 统一为有权限的接口设置headers请求头
     // 只有url中包含/my的才需要发headers
     if (options.url.indexOf('/my/') !== -1) {
